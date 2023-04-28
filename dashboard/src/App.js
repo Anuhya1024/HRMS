76c6611e68143905d71aedefffc7ed1a47df5ada
Anuhya1024 <anuhya.mandalapu@colorado.edu>
2023-04-27 20:41:13 -0600

    Initialized all necessary files.

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,11 +1,14 @@
 import React, {useEffect} from 'react';
-import { BrowserRouter, Routes, RouterProps } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
+import {Navbar, Footer, Sidebar, ThemeSettings} from './components';
+import { Employees, Kanban, Editor, Calender, ColorPicker, Line, Bar, Pie, ColorMapping, Area} from './pages';
 import './App.css';
 
 const App = () => {
+    const activeMenu = true;
   return (
     <div>
         <BrowserRouter>
@@ -20,6 +23,55 @@ const App = () => {
                             <FiSettings />
                         </button>
                     </TooltipComponent>
+                </div>
+                    {activeMenu ?(
+                        <div className="w-72 fixed sidebar
+                        dark:bg-secondary-dark-bg
+                        bg-white">
+                            Sidebar
+                        </div>
+                    ) : (
+                        <div className="w-0
+                        dark:bg-secondary-dark-bg">
+                           Sidebar
+                        </div>
+                    )}
+                    <div className={
+                        `dark:bg-main-bg bg-main-bg min-h-screen w-full ${ activeMenu ? 'md:ml-72' : 'flex-2'}`
+                    }>
+                        <div className="fixed md:static
+                        bg-main-bg dark:bg-main-dark-bg
+                        navbar w-full">
+                            Navbar
+                        </div>
+                    </div>
+                    <div>
+                        {/* Dashboard */}
+                        <Routes>
+                            <Route path="/" element="Home"/>
+                            <Route path="/Home" element="Home"/>
+                        
+
+                            {/* Pages */}
+                            <Route path="/salary" element="Salary"/>
+                            <Route path="/employees" element="Employee"/>
+
+                            {/* Apps */}
+                            <Route path="/kanban" element="Kanban"/>
+                            <Route path="/editor" element="Editor"/>
+                            <Route path="/calender" element="Calender"/>
+                            <Route path="/color-picker" element="ColorPicker"/>
+
+                            {/* Charts */}
+                            <Route path="/line" element="Line"/>
+                            <Route path="/bar" element="Bar"/>
+                            <Route path="/pie" element="Pie"/>
+
+
+                            {/* Tables */}
+                            <Route path="/basic-table" element="BasicTable"/>
+                        </Routes>
+
                     </div>
             </div>
         </BrowserRouter>
